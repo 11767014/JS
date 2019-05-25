68e231b68adb5e898f978781110c51d5825d55d4
11767014 <50442149+11767014@users.noreply.github.com>
2019-05-25 16:31:24 +0200

    Update drawCard.js

--- drawCard.js
+++ drawCard.js
@@ -11,7 +11,7 @@ function resizeGame(){
 	if(screenWidth > screenHeight){
 		size = 0.9*screenHeight;
 		} else {
-		size = 0.9*screenHeight;
+		size = 0.9*screenWidth;
 		}
 	container.style.width = container.style.height = size + "px"
 	
